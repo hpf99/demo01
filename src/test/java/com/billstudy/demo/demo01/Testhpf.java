f4b55c32c6944e54b04a73fe31b16b8519ce8577
panfeng.hou <hpf99@126.com>
2016-09-29 10:26:07 +0800

    test

---
+++ src/test/java/com/billstudy/demo/demo01/Testhpf.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 1999-2004 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+package com.billstudy.demo.demo01;
+
+/**
+ * 类Testhpf.java的实现描述：TODO 类实现描述 
+ * @author panfeng.hou 2016年9月29日 上午10:22:08
+ */
+public class Testhpf {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
